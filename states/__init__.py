7d35569ca60df436da77a3d2c3395aaa842542a8
office612 <office@salesdoc.io>
2026-03-16 16:48:53 +0500

    Update __init__.py

--- states/__init__.py
+++ states/__init__.py
@@ -17,3 +17,22 @@ class PaymentStates(StatesGroup):
 
 class AuthStates(StatesGroup):
     choosing_name = State()
+from aiogram.fsm.state import State, StatesGroup
+
+
+class PaymentStates(StatesGroup):
+    choose_month    = State()
+    choose_category = State()
+    choose_license  = State()
+    enter_client    = State()
+    enter_qty       = State()
+    choose_period   = State()
+    enter_price     = State()
+    enter_amount    = State()
+    choose_bank     = State()
+    enter_fact      = State()
+    confirm         = State()
+
+
+class AuthStates(StatesGroup):
+    choosing_name = State()
