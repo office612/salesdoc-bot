df4e1cce0a75a3972b89cc2694b8d924f4b466cb
office612 <office@salesdoc.io>
2026-03-23 15:49:36 +0600

    Add choose_manager state for accountant

--- states/__init__.py
+++ states/__init__.py
@@ -2,6 +2,31 @@ from aiogram.fsm.state import State, StatesGroup
 
 
 class PaymentStates(StatesGroup):
+    choose_manager     = State()   # бухгалтер выбирает менеджера (ВРЕМЕННО)
+    choose_month       = State()
+    choose_category    = State()
+    choose_license     = State()
+    enter_client       = State()
+    enter_qty          = State()
+    choose_period      = State()
+    choose_package     = State()   # пакет для услуг (Стандарт/Плюс/Премиум)
+    confirm_price      = State()
+    enter_price        = State()
+    enter_amount       = State()
+    choose_bank        = State()
+    enter_fact         = State()
+    choose_start_month = State()
+    choose_activation  = State()
+    choose_act_period  = State()
+    confirm            = State()
+
+
+class AuthStates(StatesGroup):
+    choosing_name = State()
+from aiogram.fsm.state import State, StatesGroup
+
+
+class PaymentStates(StatesGroup):
     choose_month       = State()
     choose_category    = State()
     choose_license     = State()
