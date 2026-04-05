b583bbdcf3ac50ea3f5d9d82b4a5c2b1e25cdf78
office612 <office@salesdoc.io>
2026-04-06 00:54:22 +0500

    Update __init__.py

--- keyboards/__init__.py
+++ keyboards/__init__.py
@@ -1,10 +1,17 @@
 from .main import main_menu, cancel_kb
-from .payment import (categories_kb, license_types_kb, periods_kb, banks_kb, confirm_kb, skip_receipt_kb)
+from .payment import (
+    categories_kb, license_types_kb, periods_kb, banks_kb, confirm_kb,
+    skip_receipt_kb, months_kb, confirm_price_kb, package_kb, payment_date_kb,
+    bot_periods_kb, manual_amount_kb, add_service_kb, back_button, service_categories_kb,
+)
 from .reports import reports_kb, months_kb, back_to_reports_kb
 
 __all__ = [
     'main_menu', 'cancel_kb',
     'categories_kb', 'license_types_kb', 'periods_kb',
     'banks_kb', 'confirm_kb', 'skip_receipt_kb',
-    'reports_kb', 'months_kb', 'back_to_reports_kb',
+    'months_kb', 'confirm_price_kb', 'package_kb',
+    'payment_date_kb', 'bot_periods_kb', 'manual_amount_kb',
+    'add_service_kb', 'back_button', 'service_categories_kb',
+    'reports_kb', 'back_to_reports_kb',
 ]
